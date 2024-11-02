349700993122e2e9ee52538d6cb9e56c28bccf0e
Abdelhadi <twitchabdelhadi@gmail.com>
2024-11-02 10:01:19 +0100

    add database tables

---
+++ database/migrations/2024_11_02_082619_create_additional_info_models_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('additional_info', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->constrained()->onDelete('cascade');
+            $table->string('address')->nullable();
+            $table->string('phone_number')->nullable();
+            $table->string('image')->nullable();
+            $table->float('rating')->nullable();
+            $table->string('hirfa')->nullable();
+            $table->date('date_of_birth')->nullable();
+            $table->enum('gender', ['male', 'female'])->nullable();
+            $table->string('city')->nullable();
+            $table->boolean('availability')->default(false);
+            $table->string('CIN')->nullable();
+            $table->text('biography')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('additional_info');
+    }
+};
